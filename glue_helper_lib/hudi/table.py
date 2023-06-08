0c0832b5f64174d599ed27abd5e007d24b346c3c
Martijn Sturm <martijn.sturm@gmail.com>
2023-06-08 17:52:26 +0200

    improve hudi glue job interface

--- glue_helper_lib/hudi/table.py
+++ glue_helper_lib/hudi/table.py
@@ -12,20 +12,20 @@ class Partitioning:
     is_datetime: bool
 
 
+class WriteMode(enum.Enum):
+    OVERWRITE = "overwrite"
+    UPSERT = "upsert"
+
+
 @dataclasses.dataclass
-class HudiTableArguments:
-    storage_location: table.StorageLocation
+class WriteHudiTableArguments:
     catalog: table.GlueCatalogArguments
     index_type: config.IndexType
     table_type: config.TableType
     record_key_colums: typing.List[str]
     precombine_column: str
     partitioning: Partitioning
-
-
-class WriteMode(enum.Enum):
-    OVERWRITE = "overwrite"
-    UPSERT = "upsert"
+    write_mode: WriteMode
 
 
 class HudiGlueTable:
@@ -34,28 +34,32 @@ class HudiGlueTable:
         WriteMode.UPSERT: "append",
     }
 
-    def __init__(self, table_arguments: HudiTableArguments) -> None:
-        self._args = table_arguments
-        self._hudi_config = config.get_hudi_options(
-            database_name=self._args.catalog.database,
-            table_name=self._args.catalog.table,
-            hudi_table_path=str(self._args.storage_location),
-            table_type=self._args.table_type,
-            index_type=self._args.index_type,
-            record_key_columns=self._args.record_key_colums,
-            precombine_column_name=self._args.precombine_column,
-            partition_key_column_name=self._args.partitioning.partition_column,
-            partitioned_on_datetime=self._args.partitioning.is_datetime,
+    def __init__(self, storage_location: table.StorageLocation) -> None:
+        self._storage_location = storage_location
+
+    def _get_hudi_config(self, write_args: WriteHudiTableArguments):
+        return config.get_hudi_options(
+            database_name=write_args.catalog.database,
+            table_name=write_args.catalog.table,
+            hudi_table_path=str(self._storage_location),
+            table_type=write_args.table_type,
+            index_type=write_args.index_type,
+            record_key_columns=write_args.record_key_colums,
+            precombine_column_name=write_args.precombine_column,
+            partition_key_column_name=write_args.partitioning.partition_column,
+            partitioned_on_datetime=write_args.partitioning.is_datetime,
         )
 
-    def write(self, df: pyspark.sql.DataFrame, write_mode: WriteMode):
-        df.write.format("hudi").options(**self._hudi_config).mode(
-            self._write_mode_to_mode_string[write_mode]
-        ).save()
+    def write(
+        self, df: pyspark.sql.DataFrame, write_args: WriteHudiTableArguments
+    ):
+        df.write.format("hudi").options(
+            **self._get_hudi_config(write_args)
+        ).mode(self._write_mode_to_mode_string[write_args.write_mode]).save()
 
     def read(
         self, spark_session: pyspark.sql.SparkSession
     ) -> pyspark.sql.DataFrame:
         return spark_session.read.format("hudi").load(
-            str(self._args.storage_location)
+            str(self._storage_location)
         )
